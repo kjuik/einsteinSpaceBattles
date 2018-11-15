1dcc7c8c8377420f6d17fa8ff9b89c2314d45a92
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2018-11-15 09:07:10 +0100

    smaceship movement. Edges of space.

---
+++ Assets/Scripts/EdgeOfSpace.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeOfSpace : MonoBehaviour
+{
+    [SerializeField]
+    EdgeOfSpace otherEdge;
+    [SerializeField]
+    bool preserveX;
+    [SerializeField]
+    bool preserveZ;
+
+    readonly List<Transform> dontSendBack = new List<Transform>();
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!dontSendBack.Contains(other.transform))
+            otherEdge.Send(other.transform);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (dontSendBack.Contains(other.transform))
+            dontSendBack.Remove(other.transform);
+    }
+    
+    public void Send(Transform sentTransform)
+    {
+        dontSendBack.Add(sentTransform);
+        
+        sentTransform.transform.position = 
+            new Vector3(
+                preserveX ? sentTransform.transform.position.x : transform.position.x,
+                sentTransform.transform.position.y, 
+                preserveZ ? sentTransform.transform.position.z : transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float Speed = 5f;
+    [SerializeField]
+    Transform visuals;
+
+    Rigidbody cachedRigidbody;
+    
+    void Awake()
+    {
+        cachedRigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        var acceleration = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0f,
+            Input.GetAxis("Vertical")
+        ) * Speed;
+
+        cachedRigidbody.AddForce(acceleration);
+
+        visuals.LookAt(visuals.position + acceleration);
+    }
+}
