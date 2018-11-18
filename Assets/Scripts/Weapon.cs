f355cd22ae6fe181ee559337d7ea0da5450b52f0
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2018-11-18 23:51:01 +0100

    delay for shots

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,13 +5,19 @@ public class Weapon : MonoBehaviour
     //todo pooling
     [SerializeField] private Projectile projectilePrefab;
     [SerializeField] private float shotFrequency;
-
+    [SerializeField] private float delay = 0f;
+    
     float lastShotTimestamp;
     
     bool CanShoot=> lastShotTimestamp + shotFrequency <= Time.time;
 
     protected virtual bool ShouldShoot { get; }
 
+    void Awake()
+    {
+        lastShotTimestamp = Time.time - shotFrequency + delay;
+    }
+    
     void Update()
     {
         if (CanShoot && ShouldShoot)
