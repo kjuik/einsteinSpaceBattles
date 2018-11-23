6151d71bfd4b79d34d1b1654c5271acd1040bf71
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2018-11-23 09:20:13 +0100

    explosions integrated?

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 0.5f;
-
+    [SerializeField] GameObject explosionPrefab;
+    
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(explosion.gameObject, explosionPrefab.GetComponentInChildren<ParticleSystem>().main.duration);
+        Destroy(gameObject);
     }
 }
