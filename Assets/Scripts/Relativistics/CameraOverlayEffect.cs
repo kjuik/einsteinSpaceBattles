d4218a2a54b4eba0dfb4b8253b8d630b7afe71f1
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2018-11-24 10:47:46 +0100

    camera overlay effect

---
+++ Assets/Scripts/Relativistics/CameraOverlayEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOverlayEffect : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private Transform perceiver;
+    
+    private Camera cam;
+    private float radius = 0f;
+    
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        radius += Time.deltaTime * Relativistics.C;
+    }
+    
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        var center = cam.WorldToScreenPoint(perceiver.position);
+        var screenRadius =
+            (center - cam.WorldToScreenPoint(perceiver.position + Vector3.right * radius)).magnitude;
+        
+        var invertedYCenter = new Vector3(center.x, Screen.height - center.y, center.z);
+        
+        material.SetVector("_Center", invertedYCenter);
+        material.SetFloat("_Radius", screenRadius);
+        
+        Graphics.Blit(src, dest, material);
+    }
+}
